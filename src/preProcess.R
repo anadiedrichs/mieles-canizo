c289b69a491d8f8be5184e284e9e8a8193363ba5
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2018-07-11 14:44:46 -0300

    resolve #8 close #8

--- src/preProcess.R
+++ src/preProcess.R
@@ -16,19 +16,19 @@ colnames(dataset)
 #' Cuántos ejemplos tenemos de cada zona
 table(dataset[1])
 dataset$`Zona geográfica` <- as.factor(dataset$`Zona geográfica`)
-names(dataset)[1] <- "Zona"
+names(dataset)[1] <- "Zona_"
 
 #' agregar columna etiqueta label para indicar si es o no de Valle de Uco para futura clasificación binaria
 
 vDeUco <- "Valle de Uco"
 add_column(dataset,label=0)
-dataset[dataset$Zona == vDeUco,"label"] <- 1
-dataset[dataset$Zona != vDeUco,"label"] <- -1
+dataset[dataset$Zona_ == vDeUco,"Region"] <- "Valle_de_Uco"
+dataset[dataset$Zona_ != vDeUco,"Region"] <- "Rest_of_Mza"
 
 #' ## Boxplot de distribución de variables por zona
-plot_boxplot(dataset,by="Zona")
+#plot_boxplot(dataset,by="Zona_")
 #' ## Boxplot de distribución de variables por si está o no en zona Valle de Uco
-plot_boxplot(dataset,by="label")
+plot_boxplot(dataset,by="Region")
 
 #' ## Correlaciones (lineales) entre las variables
 #' 
@@ -41,16 +41,23 @@ data <-  dataset[-quitar]
 media <- apply(data, 2, mean)
 std <- apply(data, 2, sd)
 data <- scale(data, center = media, scale = std)
-data <- as.data.frame(cbind(data,label=dataset$label))
+data <- as.data.frame(cbind(data,Region=dataset$Region))
+#data[-ncol(data)] <- sapply(data[-ncol(data)],as.numeric)
+#data$Region <- as.factor(data$Region)
 
-#'  Boxplot de distribución de variables por si está o no en zona Valle de Uco
-plot_boxplot(data,by="label")
+
+#'  Boxplot de distribución de variables por si está o no en zona Valle de Uco.
+#'  Los datos en este plot están normalizados. 
+#'  Gráfico candidato para el paper
+plot_boxplot(data,by="Region")
 
 #' ### Feature plot, pairs of variables
-featurePlot(x=dataset[,2:6], y=as.factor(dataset$label),  "pairs", auto.key=list(columns=2))
-my_cols <- c("#00AFBB", "#E7B800", "#FC4E07")  
+#' Plot de variables tomadas de a pares. Los datos han sido normalizados.
+#' Se toman unas seis variables a modo de ejemplo del plot.
+featurePlot(x=data[,2:6], y=as.factor(data$Region),  "pairs", auto.key=list(columns=2))
+my_cols <- c("#00AFBB", "#FC4E07","#E7B800")  
 pairs(dataset[,2:7], pch = 19,  cex = 0.5,
-      col = my_cols[dataset$Zona],
+      col = my_cols[data$Region],
       lower.panel=NULL)
 
 #' Li y Na parecieran ser variables candidatas de mayor peso en el modelo clasificador.
