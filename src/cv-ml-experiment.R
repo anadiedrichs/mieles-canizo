003f3373714e1d7f95ee38c4f3a8cd21bcdaed8d
Ana Diedrichs (yoga) <ana.diedrichs@frm.utn.edu.ar>
2018-07-12 11:48:32 -0300

    resumen resultados finales en tabla

--- src/cv-ml-experiment.R
+++ src/cv-ml-experiment.R
@@ -25,15 +25,16 @@ print(model.lm$finalModel)
 pred <- predict(model.lm,test)
 pred[which(pred > 0)] <- 1
 pred[which(pred <= 0)] <- -1
-confusionMatrix(as.factor(pred), as.factor(test$label))
-
+c <- confusionMatrix(as.factor(pred), as.factor(test$label),mode = "prec_recall")
+print(c)
+table.results <- as.data.frame(cbind(model="lm",t(c$overall),t(c$byClass)))
 
 #' ## RANDOM FOREST MODEL
 set.seed(825)
 model.rf <- train(as.factor(label)~., data=train, 
                   trControl=train_control, method="rf",metric="Accuracy")
 
-#' ### Results of random forest model
+#'  ### Results of random forest model
 print(model.rf)
 plot(model.rf)
 print(model.rf$finalModel)
@@ -42,8 +43,10 @@ varImp(model.rf)
 plot(varImp(model.rf))
 #' Predicción en test-set
 pred <- predict(model.rf,test)
-confusionMatrix(pred, as.factor(test$label))
-
+c <- confusionMatrix(as.factor(pred), as.factor(test$label),mode = "prec_recall")
+print(c)
+d <- as.data.frame(cbind(model="rf",t(c$overall),t(c$byClass)))
+table.results <- rbind(table.results,d)
 
 #' ## CTree or conditional inference tree
 set.seed(825)
@@ -56,7 +59,10 @@ print(model.ctree$finalModel)
 plot(model.ctree$finalModel)
 #' Predicción en test-set
 pred <- predict(model.ctree,test)
-confusionMatrix(pred, as.factor(test$label))
+c <- confusionMatrix(as.factor(pred), as.factor(test$label),mode = "prec_recall")
+print(c)
+d <- as.data.frame(cbind(model="ctree",t(c$overall),t(c$byClass)))
+table.results <- rbind(table.results,d)
 
 #' ## LDA o linear discriminant analysis
 #' 
@@ -68,14 +74,35 @@ print(model.lda)
 print(model.lda$finalModel)
 #' Predicción en test-set
 pred <- predict(model.lda,test)
-confusionMatrix(pred, as.factor(test$label))
+c <- confusionMatrix(as.factor(pred), as.factor(test$label),mode = "prec_recall")
+print(c)
+d <- as.data.frame(cbind(model="LDA",t(c$overall),t(c$byClass)))
+table.results <- rbind(table.results,d)
+
+#' ## C5.0
+#' 
+set.seed(825)
+library(C50)
+model.c50 <- train(as.factor(label)~., data=train, 
+                   trControl=train_control, method="C5.0",metric="Accuracy")
+print(model.c50)
+#' modelo final 
+print(model.c50$finalModel)
+#' Predicción en test-set
+pred <- predict(model.c50,test)
+c <- confusionMatrix(as.factor(pred), as.factor(test$label),mode = "prec_recall")
+print(c)
+d <- as.data.frame(cbind(model="C5.0",t(c$overall),t(c$byClass)))
+table.results <- rbind(table.results,d)
 
 #' ## Comparación de todos los modelos en test.set
 #' 
-results <- resamples(list(RF=model.rf,ctree=model.ctree,LDA=model.lda))
+results <- resamples(list(RF=model.rf,ctree=model.ctree,LDA=model.lda,C5.0=model.c50))
 # summarize the distributions
 summary(results)
 # boxplots of results
 bwplot(results)
 # dot plots of results
 dotplot(results)
+#' Tabla final de resultados
+print(table.results)
